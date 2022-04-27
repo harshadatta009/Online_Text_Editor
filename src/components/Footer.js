4cfb2f7cf290a7771be824955e45720c4c0032ba
harshadatta009 <harsha_datta@yahoo.com>
2022-04-27 22:09:56 +0530

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = props => {
+  return (
+    <div className='footer'>
+      <p className='d-flex align-items-center justify-content-center text-white bg-black'>
+        &#169;Done by harsha datta
+      </p>
+    </div>
+  )
+}
+
+export default Footer
